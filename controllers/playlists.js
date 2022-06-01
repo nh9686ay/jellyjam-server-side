669ba96f74b6e778303e75b087fa184896243639
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-06-01 16:19:16 -0400

    seed db from spotify api, res.json db data

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -24,40 +24,66 @@ router.get('/', (req, res) => {
         .catch(console.error);
 })
 
+
 router.get('/fetchtest', (req, res) => {
-    async function fetchData() {
-         
-        const params = new URLSearchParams();
+    const params = new URLSearchParams();
         params.append('grant_type', 'client_credentials');
-        const res = await axios.post('https://accounts.spotify.com/api/token', params, {
-          headers: {
-            'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')),
-            'Content-Type': "application/x-www-form-urlencoded"
-          },
-        })
-        console.log(res.data.access_token)
-      //   await setToken(res.data.access_token)
-
-        const token = res.data.access_token
-        const { data } = await axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
+        const result = axios.post('https://accounts.spotify.com/api/token', params, {
             headers: {
-                'Authorization': `Bearer ${token}`
+                'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')),
+                'Content-Type': "application/x-www-form-urlencoded"
             },
-            params: {
-                // q: searchKey,
-                // type: 'album'
-            }
         })
-        console.log(data)
-  
-        // setFeaturedPlaylists(data.playlists.items)
-        
-    }
+        .then((playlist) => {
+
+            const token = playlist.data.access_token
+            axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                params: {
+                    // q: searchKey,
+                    // type: 'album'
+                }
+            })
+            .then(result => {
     
-    fetchData();
+                Playlist.deleteMany({})
+                    .then(() => {
+                        for(let i = 0; i < result.data.playlists.items.length; i++) {
 
+                            Playlist.insertMany({
+                                description: result.data.playlists.items[i].description,
+                                external_url: result.data.playlists.items[i].external_urls.spotify,
+                                SPId: result.data.playlists.items[i].id,
+                                image_url: result.data.playlists.items[i].images[0].url,
+                                name: result.data.playlists.items[i].name,
+                                owner: {
+                                    name: result.data.playlists.items[i].owner.display_name, //item.owner.display_name
+                                    url: result.data.playlists.items[i].owner.external_urls.spotify,  //item.owner.external_urls.spotify
+                                },
+                                totalTracks: result.data.playlists.items[i].tracks.total,
+                            })
+                            .then(() => {
+                                console.log('added playlists')
+                            })
+                            .catch(console.error)
+                        }
+                    })
+                    .catch(console.error)
+            })
+            .catch(console.error)
+        })
+        .then(() => {
+            Playlist.find()
+            .then((playlists) => {
+                res.json(playlists)
+            })
+            .catch(console.error)
+        })
+        .catch(console.error)
+    
 })
-
 
 
 
