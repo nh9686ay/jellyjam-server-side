c259c7157c6e319f6fe1721eb32534ff5d947fa5
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-05-31 23:45:38 -0400

    console logged playlists, need to refactor

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -1,10 +1,19 @@
 
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
 
 const Playlist = require('../db/models/playlistModel');
 
 const router = express.Router();
 
+router.use(cors());
+
+
+// make into env vars
+const client_id = '638824d8d1cf48bca579d7fa24c5ac40';
+const client_secret = 'cb118a82d49d4d51b6f2e5ecefed9085';
 
 
 router.get('/', (req, res) => {
@@ -13,6 +22,40 @@ router.get('/', (req, res) => {
             res.json(playlists)
         })
         .catch(console.error);
+})
+
+router.get('/fetchtest', (req, res) => {
+    async function fetchData() {
+         
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        const res = await axios.post('https://accounts.spotify.com/api/token', params, {
+          headers: {
+            'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')),
+            'Content-Type': "application/x-www-form-urlencoded"
+          },
+        })
+        console.log(res.data.access_token)
+      //   await setToken(res.data.access_token)
+
+        const token = res.data.access_token
+        const { data } = await axios.get('https://api.spotify.com/v1/browse/featured-playlists', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            params: {
+                // q: searchKey,
+                // type: 'album'
+            }
+        })
+        console.log(data)
+  
+        // setFeaturedPlaylists(data.playlists.items)
+        
+    }
+    
+    fetchData();
+
 })
 
 
